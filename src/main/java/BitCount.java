d0fcf6a23f21ac420ba26630e24449a7f6c27791
Max50204 <schneidermaxim45@yahoo.com>
2024-12-18 08:58:44 +0100

    18_12

---
+++ src/main/java/BitCount.java
@@ -0,0 +1,10 @@
+public class BitCount {
+    public static int countOnes(int n) {
+        return Integer.bitCount(n);
+    }
+
+    public static void main(String[] args) {
+        int n = 10; // пример
+        System.out.println("Количество единиц в двоичном представлении: " + countOnes(n));
+    }
+}
---
+++ src/main/java/Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+    public static void main(String[] args) {
+        counter(50);
+    }
+
+    public static void counter(int n){
+
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        counter(n-2);
+    }
+
+}
